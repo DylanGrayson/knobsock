ab3398be0e264c2425141a61f1d854ffa62abea0
Dylan Grayson <dgrayson11@gmail.com>
2015-04-16 19:18:39 -0700

    groups api working with one member in members

--- main.py
+++ main.py
@@ -7,85 +7,104 @@ import model
 
 
 def UserAsDict(user):
-  return {
-    'id': user.user_id(), 
-    'email': user.email(), 
-    'admin': users.is_current_user_admin(),
-    'nickname': user.nickname()
-  }
+    return {
+        'id': user.user_id(),
+        'email': user.email(),
+        'admin': users.is_current_user_admin(),
+        'nickname': user.nickname()
+    }
+
 
 def GroupsAsDict(groups):
-  group_dict = {'groups': []}
-  for group in groups:
-    group_dict.groups.append({'name': group.name()})
-  return group_dict
+    group_dict = {'groups': []}
+    for group in groups:
+        new_group = {'name': group.name, 'owner': UserAsDict(group.members[0])}
+        group_dict["groups"].append(new_group)
+    return group_dict
 
-# parent handler class 
+# parent handler class
+
+
 class RestHandler(webapp2.RequestHandler):
 
-  def dispatch(self):
-    #time.sleep(1)
-    super(RestHandler, self).dispatch()
+    def dispatch(self):
+        # time.sleep(1)
+        super(RestHandler, self).dispatch()
 
+    def SendJson(self, r):
+        self.response.headers['content-type'] = 'text/plain'
+        self.response.write(json.dumps(r))
 
-  def SendJson(self, r):
-    self.response.headers['content-type'] = 'text/plain'
-    self.response.write(json.dumps(r))
-    
 
 class UserHandler(RestHandler):
 
-  def get(self):
-    if "login" in self.request.path:
-      self.redirect(users.create_login_url())
-    elif "logout" in self.request.path:
-      self.redirect(users.create_logout_url('/'))
-    else:
-        user = users.get_current_user()
-        if user != None:
-          self.SendJson(UserAsDict(user))
+    def get(self):
+        if "login" in self.request.path:
+            self.redirect(users.create_login_url())
+        elif "logout" in self.request.path:
+            self.redirect(users.create_logout_url('/'))
         else:
-          self.SendJson({'user': None})
+            user = users.get_current_user()
+            if user != None:
+                self.SendJson(UserAsDict(user))
+            else:
+                self.SendJson({'user': None})
+
 
 class GroupHandler(RestHandler):
 
-  def get(self):
-    user = users.get_current_user()
-    if user != None:
-      groups = model.ListGroups(user)
-      self.SendJson(GroupsAsDict(groups))
+    def get(self):
+        user = users.get_current_user()
+        if user != None:
+            groups = model.ListGroups(user)
+            self.SendJson(GroupsAsDict(groups))
+
+
+class GroupCreateHandler(RestHandler):
+
+    def get(self):
+      user = users.get_current_user()
+      if user != None:
+        group1 = model.Group(name="clown", members=[user])
+        #UNCOMMENT BELOW to put a test group in under your username
+        #group1.put()
+
+    def post(self):
+        r = json.loads(self.request.body)
+        group = model.CreateGroup(r['group_name'])
+
 
 class QueryHandler(RestHandler):
 
-  def get(self):
-    guests = model.AllGuests()
-    r = [ AsDict(guest) for guest in guests ]
-    self.SendJson(r)
+    def get(self):
+        guests = model.AllGuests()
+        r = [AsDict(guest) for guest in guests]
+        self.SendJson(r)
 
 
 class UpdateHandler(RestHandler):
 
-  def post(self):
-    r = json.loads(self.request.body)
-    guest = model.UpdateGuest(r['id'], r['first'], r['last'])
-    r = AsDict(guest)
-    self.SendJson(r)
+    def post(self):
+        r = json.loads(self.request.body)
+        guest = model.UpdateGuest(r['id'], r['first'], r['last'])
+        r = AsDict(guest)
+        self.SendJson(r)
 
 
 class InsertHandler(RestHandler):
 
-  def post(self):
-    r = json.loads(self.request.body)
-    guest = model.InsertGuest(r['first'], r['last'])
-    r = AsDict(guest)
-    self.SendJson(r)
+    def post(self):
+        r = json.loads(self.request.body)
+        guest = model.InsertGuest(r['first'], r['last'])
+        r = AsDict(guest)
+        self.SendJson(r)
 
 
 class DeleteHandler(RestHandler):
 
-  def post(self):
-    r = json.loads(self.request.body)
-    model.DeleteGuest(r['id'])
+    def post(self):
+        r = json.loads(self.request.body)
+        model.DeleteGuest(r['id'])
 
 
 APP = webapp2.WSGIApplication([
@@ -94,7 +113,6 @@ APP = webapp2.WSGIApplication([
     ('/api/delete', DeleteHandler),
     ('/api/update', UpdateHandler),
     (r'/api/user/.*', UserHandler),
+    (r'/api/groups/create', GroupCreateHandler),
     (r'/api/groups/.*', GroupHandler),
 ], debug=True)
-
-

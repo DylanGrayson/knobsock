b14734a9d5f5cdb991c831b6b95d9d82a4e51d9b
Conner Swann <me@connerswann.me>
2015-04-29 21:54:24 -0700

    cleaned up dashboard templates for knobs and got it drawing the UI with information retrieved from the API
    next step, clean it up and make the inactive knobs able to post back to the server

--- static-html/app.js
+++ static-html/app.js
@@ -1,6 +1,8 @@
 
 var currentGroup = 0;
 
+var current_groups = [];
+
 var group_source = $("#group_template").html();
 var group_template = Handlebars.compile(group_source);
 var group_context = {};
@@ -13,9 +15,14 @@ var active_sock_source = $('#active-sock-template').html();
 var active_sock_template = Handlebars.compile(active_sock_source);
 var active_sock_context = {};
 
+var inactive_sock_source = $('#inactive-sock-template').html();
+var inactive_sock_template = Handlebars.compile(inactive_sock_source);
+var inactive_sock_context = {};
+
 fetchGroups = function() {
     $.getJSON("/api/groups/s.json", function(data) {
         group_context['groups'] = data.groups;
+        current_groups = data.groups;
         member_context['members'] = data.groups[currentGroup].members;
         member_context['curGroup'] = data.groups[currentGroup].name;
     });
@@ -35,7 +42,34 @@ change_group = function(index) {
     document.getElementById('member_list').innerHTML = member_html;
 }
 
-main_loop = function(){
+updateSocks = function() {
+    $('#sock-container').html("");
+    for (var i = 0; i < current_groups.length; i++) {
+        if (current_groups[i].knob == true) {
+            var sock_context = {
+                'group_name': current_groups[i].name,
+                'time_remaining': current_groups[i].timeout - current_groups[i].servertime
+            }
+            var sock_html = active_sock_template(sock_context);
+            $('#sock-container').append(sock_html);
+        }
+        
+    }
+    for (var i = 0; i < current_groups.length; i++) {
+        if (current_groups[i].knob != true) {
+            var sock_context = {
+                'group_name': current_groups[i].name,
+                'time_remaining': current_groups[i].timeout - current_groups[i].servertime,
+                'percentage_remaining': (current_groups[i].timein / current_groups[i].timeout) * 100
+            }
+            var sock_html = inactive_sock_template(sock_context);
+            $('#sock-container').append(sock_html);
+        }
+        
+    }
+}
+
+main_loop = function() {
     //$.material.init();
     $(".slider").noUiSlider({
         start: 15,
@@ -52,7 +86,9 @@ main_loop = function(){
 
 
     fetchGroups();
+    
     setInterval(fetchGroups, 5000);
+    setTimeout(updateSocks, 3000);
 };
 
 main_loop();
